5ce3c362c801e527308ed729db260134d5ff7122
Ayoub Aharmim <33521285+ayoubaharmim@users.noreply.github.com>
2020-09-28 18:13:03 +0100

    create global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import {GlobalStyles} from './global-styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <App />
+    <GlobalStyles/>
+  </>
+  , document.getElementById('root')
+);

6b32e11930a725ab351ccfd6cf01e7741c928cb1
Ayoub Aharmim <33521285+ayoubaharmim@users.noreply.github.com>
2020-09-28 18:19:46 +0100

    import normalize

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import {GlobalStyles} from './global-styles';
+import 'normalize.css';
 
 ReactDOM.render(
   <>
